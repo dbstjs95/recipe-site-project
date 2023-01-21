f5df4b8c4349137b32e526cc373b9a4e0c070e14
dbstjs95 <choji95@naver.com>
2023-01-22 06:31:36 +0900

    add event img

--- client/src/mockData/event_list.js
+++ client/src/mockData/event_list.js
@@ -1,14 +1,5 @@
-import event1 from "../components/event_img/img01.png";
-import event2 from "../components/event_img/img02.png";
-import event3 from "../components/event_img/img03.png";
-import event4 from "../components/event_img/img04.png";
 import e1 from "../components/event_img/e1.png";
 import e2 from "../components/event_img/e2.png";
+import e3 from "../components/event_img/e3.png";
 
-export const events = [
-  // { src: event1 },
-  // { src: event2 },
-  // { src: event3 },
-  { src: e1 },
-  { src: e2 },
-];
+export const events = [{ src: e1 }, { src: e2 }, { src: e3 }];
